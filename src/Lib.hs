859cc8f8bae9f2fdde711838c7c03aaad6d5c6da
Matthias Treydte <mt@waldheinz.de>
2019-11-11 21:34:02 +0100

    define some basic data types

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,5 +2,27 @@ module Lib
     ( someFunc
     ) where
 
+import qualified Data.HashMap.Strict as HMS
+
+data Course = Course
+    { courseName :: String
+    , courseMaxStudents :: Int
+    } deriving ( Show, Eq )
+
+newtype CourseId = CourseId { unCourseId :: Int }
+    deriving ( Show, Eq )
+
+newtype CourseMap = CourseMap { unCourseMap :: HMS.HashMap CourseId Course }
+    deriving ( Show, Eq )
+
+data Student = Student
+    { studFirstName :: String
+    , studLastName :: String
+    , studClass :: String
+    , studPreference :: [ CourseId ]
+    } deriving ( Show, Eq )
+
+newtype Individual = Individual { unIndividual :: HMS.HashMap CourseId [ Student ] }
+
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
