c58ebe3a3fbe321223377cdbff70552e826b4453
Matthias Treydte <mt@waldheinz.de>
2019-11-11 22:20:27 +0100

    generate random individuals / population

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,8 +1,14 @@
+{-# LANGUAGE DeriveGeneric #-}
+
 module Lib
     ( someFunc
     ) where
 
+import Control.Monad ( replicateM )
+import Control.Monad.Random ( Rand, RandomGen, getRandomR, evalRandIO )
+import Data.Hashable ( Hashable )
 import qualified Data.HashMap.Strict as HMS
+import GHC.Generics (Generic)
 
 data Course = Course
     { courseName :: String
@@ -10,7 +16,9 @@ data Course = Course
     } deriving ( Show, Eq )
 
 newtype CourseId = CourseId { unCourseId :: Int }
-    deriving ( Show, Eq )
+    deriving ( Show, Eq, Generic )
+
+instance Hashable CourseId
 
 newtype CourseMap = CourseMap { unCourseMap :: HMS.HashMap CourseId Course }
     deriving ( Show, Eq )
@@ -22,7 +30,41 @@ data Student = Student
     , studPreference :: [ CourseId ]
     } deriving ( Show, Eq )
 
-newtype Individual = Individual { unIndividual :: HMS.HashMap CourseId [ Student ] }
+type Students = [ Student ]
+
+newtype Individual = Individual { unIndividual :: HMS.HashMap CourseId Students }
+    deriving ( Show )
+
+randomIndividual :: (RandomGen g)
+    => CourseId -- ^ max course ID, IDs must be in [ 1 .. maxCid ]
+    -> Students
+    -> Rand g Individual
+randomIndividual maxCid students = Individual <$> result
+    where
+        result = HMS.fromListWith (++) <$> mapM go students
+        go student = getRandomR (1, unCourseId maxCid) >>=
+            \cid -> pure (CourseId cid, [ student ])
+
+newtype Population = Population { unPopulation :: [ Individual ] }
+    deriving ( Show )
+
+randomPopulation :: (RandomGen g)
+    => Int -- ^ population size
+    -> CourseId -- ^ see randomIndividual
+    -> Students
+    -> Rand g Population
+randomPopulation ps maxCid students = Population <$> replicateM ps (randomIndividual maxCid students)
 
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = do
+    let
+        studs =
+            [ Student "Falko" "Schleif" "3b" [ ]
+            , Student "Matthias" "Treydte" "2b" [ ]
+            , Student "Helene" "Fischer" "4a" [ ]
+            , Student "Summer" "Glau" "3b" [ ]
+            , Student "Sarah" "Connor" "7a" [ ]
+            ]
+
+    values <- evalRandIO (randomPopulation 1 (CourseId 3) studs)
+    putStrLn (show values)
